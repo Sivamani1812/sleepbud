dd10b002bde0e92d5d629d5445b1429875ea6898
Sivamani1812 <152249643+Sivamani1812@users.noreply.github.com>
2025-11-27 10:27:48 -0500

    update1

---
+++ assets/js/wow.js
@@ -0,0 +1,12 @@
+new WOW().init();
+
+const footerLinks = document.querySelectorAll(".footer-links a");
+
+   footerLinks.forEach(link => {
+     link.addEventListener("mouseover", () => {
+       link.classList.add("animate__animated", "animate__headShake");
+     });
+     link.addEventListener("animationend", () => {
+       link.classList.remove("animate__animated", "animate__headShake");
+     });
+   });
